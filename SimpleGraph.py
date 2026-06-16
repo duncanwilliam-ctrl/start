1515c426e7be6e586bc6a692879687a865cfb957
William Duncan <duncan.william@gmail.com>
2026-06-15 22:32:33 -0400

    simple graph new line characteristics and graph elements.

--- SimpleGraph.py
+++ SimpleGraph.py
@@ -3,13 +3,18 @@ import matplotlib.pyplot as plt
 
 
 x = [1, 2, 3, 4, 5]
-y = [2, 3, 4, 5, 6]
+y = [20, 30, 40, 50, 52]
+x2 = [6, 8, 10, 12, 14]
+y2 = [10, 20, 30, 40, 70]
 
+plt.plot(x, y, c="orange", linewidth=3, marker='o',
+         markersize=8, label='Line 1', linestyle='--')
+plt.plot(x2, y2, c="blue", linewidth=0.5,
+         marker='s', markersize=8, label='Line 2')
 
-plt.plot(x, y)
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
 plt.title('Simple Graph')
+plt.legend()
 
-
-plt.show()
+plt.show()  # this will show the plot in a window
